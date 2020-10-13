892dbeb1d24eda949de622b7a10f91d30ac977af
Benedikt Böhme <benediktbhm@gmail.com>
2020-10-13 18:04:01 +0200

    Moved stats gathering to new file

--- 09 - Preprocess the AmazonCat-13k Dataset.py
+++ 09 - Preprocess the AmazonCat-13k Dataset.py
@@ -1,9 +1,7 @@
 # %%
-from utils.dataset import get_stats, class_frequencies
 from utils.text_preprocessing import preprocess
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 # %% [markdown]
 # # Preprocess the AmazonCat-13k Dataset
@@ -14,76 +12,17 @@ DATASET_TYPE = 'trn'
 ADD_CONTENT = False
 # CUTOFF is inclusive
 CUTOFF = 0 if DATASET_TYPE == 'trn' else 0
-INPUT_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.json'
-OUTPUT_SUFFIX = '.content' if ADD_CONTENT else ''
-OUTPUT_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}{OUTPUT_SUFFIX}.processed.json'
+DS_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.json'
+SUFFIX = '.content' if ADD_CONTENT else ''
+BEFORE_CUTOFF_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}{SUFFIX}.before.json'
+AFTER_CUTOFF_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}{SUFFIX}.processed.json'
 
 # %% [markdown]
 # First, load the dataset.
 
 # %%
-df = pd.read_json(INPUT_PATH, lines=True)
+df = pd.read_json(DS_PATH, lines=True)
 df
-
-# %% [markdown]
-# The dataset has the fields: *uid*, *title*, *content*, *target_ind*, *target_rel*.
-# Next, we can calculate the maximum and minimum inds.
-
-# %%
-max_ind = 0
-min_ind = 2_147_483_647
-for inds in df['target_ind']:
-    a = np.array(inds)
-    ma = a.max()
-    if ma > max_ind:
-        max_ind = ma
-    mi = a.min()
-    if mi < min_ind:
-        min_ind = mi
-
-print("Max ind:", max_ind)
-print("Min ind:", min_ind)
-print("Count (= difference + 1):", max_ind - min_ind + 1)
-print("Count (expected):", CLASS_COUNT)
-
-# %% [markdown]
-# Next, we can calculate the statistics for class frequencies, title char lengths, content char lengths, and instance class counts.
-
-# %%
-vlen = np.vectorize(len)
-
-freqs = class_frequencies(CLASS_COUNT, df['target_ind'])
-
-stats = [
-    ('title char lengths', get_stats(vlen(df['title']))),
-    ('content char lengths', get_stats(vlen(df['content']))),
-    ('class frequencies', get_stats(freqs)),
-    ('instance class count', get_stats(df['target_ind'].map(len))),
-]
-
-ds_stats_index, ds_stats = zip(*stats)
-pd.DataFrame(ds_stats, index=ds_stats_index)
-
-# %% [markdown]
-# Create a boxplot for the frequencies.
-
-# %%
-fig1, ax1 = plt.subplots()
-ax1.set_title('Class frequencies')
-ax1.boxplot(freqs)
-plt.savefig(f'datasets/imgs/AmazonCat-13K_{DATASET_TYPE}_boxplot.png', dpi=163)
-
-# %% [markdown]
-# Create a historgram for the frequencies.
-
-# %%
-sorted = np.sort(freqs)
-
-fig2, ax2 = plt.subplots()
-ax2.set_title('Class frequencies')
-plt.yscale('log')
-ax2.plot(np.arange(sorted.shape[0]), sorted)
-plt.savefig(f'datasets/imgs/AmazonCat-13K_{DATASET_TYPE}_histogram.png', dpi=163)
 
 # %% [markdown]
 # ## Preprocess the Dataset
@@ -95,15 +34,14 @@ X = preprocess(df['title'])
 y = df['target_ind']
 
 # %% [markdown]
-# Next, we can calculate the statistics for the token lengths.
+# Save the dataset before the cutoff
+
+df_processed = pd.DataFrame({ 'X': X, 'y': y })
+df_processed.to_json(BEFORE_CUTOFF_PATH, orient='records', lines=True)
 
 # %%
+vlen = np.vectorize(len)
 token_lens = vlen(X)
-
-stats.append(('token lengths', get_stats(token_lens)))
-
-ds_stats_index, ds_stats = zip(*stats)
-pd.DataFrame(ds_stats, index=ds_stats_index)
 
 # %% [markdown]
 # Cut off instances with not enough tokens.
@@ -120,45 +58,6 @@ X = X[indices]
 y = y[indices]
 
 # %% [markdown]
-# Calculate the metrics after the cutoff.
-
-# %%
-token_lens = vlen(X)
-
-stats.extend([
-    ('class frequencies (after cutoff)', get_stats(class_frequencies(CLASS_COUNT, y))),
-    ('instance class count (after cutoff)', get_stats(y.map(len))),
-    ('token lengths (after cutoff)', get_stats(token_lens)),
-])
-
-ds_stats_index, ds_stats = zip(*stats)
-stats_df = pd.DataFrame(ds_stats, index=ds_stats_index)
-stats_df.to_csv(f'datasets/stats/AmazonCat-13K_{DATASET_TYPE}{OUTPUT_SUFFIX}.csv')
-
-# %% [markdown]
-# Calculate the labels below a set of frequency thresholds
-
-# %%
-freqs_args = np.argsort(freqs)
-
-def freqs_args_below(threshold):
-    # Index before which all indexes point to frequences below the threshold
-    i = np.searchsorted(freqs, threshold, side='right', sorter=freqs_args)
-    # return freqs_args[i-1:0:-1]
-    return freqs_args[i-1]
-
-vfreqs_args_below = np.vectorize(freqs_args_below)
-
-# %%
-thresholds = [50, 100, 1_000, 10_000, 50_000, 100_000]
-labels = vfreqs_args_below(thresholds)
-
-threshold_labels = pd.DataFrame(
-    { 'label': labels, 'frequency': freqs[labels] },
-    index=pd.Index(thresholds, name='threshold'))
-threshold_labels.to_csv(f'datasets/stats/AmazonCat-13K_{DATASET_TYPE}_thresholds.csv')
-
-# %% [markdown]
 # Save the dataset
 
 # %%
@@ -166,4 +65,4 @@ df_processed = pd.DataFrame({ 'X': X, 'y': y })
 df_processed
 
 # %%
-df_processed.to_json(OUTPUT_PATH, orient='records', lines=True)
+df_processed.to_json(AFTER_CUTOFF_PATH, orient='records', lines=True)
---
+++ 14 - Gather statistics for the AmazonCat-13k Dataset.py
@@ -0,0 +1,120 @@
+# %%
+from utils.dataset import get_stats, class_frequencies
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% [markdown]
+# # Gather Statistics for the AmazonCat-13k Dataset
+
+# %%
+CLASS_COUNT = 13330
+DATASET_TYPE = 'trn'
+ADD_CONTENT = False
+RAW_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.json'
+BEFORE_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.before.json'
+AFTER_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.processed.json'
+
+# %% [markdown]
+# First, load the dataset.
+
+# %%
+df_raw = pd.read_json(RAW_PATH, lines=True)
+df_raw
+
+# %%
+df_before = pd.read_json(BEFORE_PATH, lines=True)
+df_after = pd.read_json(AFTER_PATH, lines=True)
+
+# %% [markdown]
+# The dataset has the fields: *uid*, *title*, *content*, *target_ind*, *target_rel*.
+# Next, we can calculate the maximum and minimum inds.
+
+# %%
+max_ind = 0
+min_ind = 2_147_483_647
+for inds in df_before['y']:
+    a = np.array(inds)
+    ma = a.max()
+    if ma > max_ind:
+        max_ind = ma
+    mi = a.min()
+    if mi < min_ind:
+        min_ind = mi
+
+print("Max ind:", max_ind)
+print("Min ind:", min_ind)
+print("Count (= difference + 1):", max_ind - min_ind + 1)
+print("Count (expected):", CLASS_COUNT)
+
+# %%
+freqs_before = class_frequencies(CLASS_COUNT, df_before['y'])
+freqs_after = class_frequencies(CLASS_COUNT, df_after['y'])
+
+# %% [markdown]
+# Create a boxplot for the frequencies.
+
+# %%
+fig1, ax1 = plt.subplots()
+ax1.set_title('Class frequencies')
+ax1.boxplot(freqs_before)
+plt.savefig(f'datasets/imgs/AmazonCat-13K_{DATASET_TYPE}_boxplot.png', dpi=163)
+
+# %% [markdown]
+# Create a historgram for the frequencies.
+
+# %%
+sorted = np.sort(freqs_before)
+
+fig2, ax2 = plt.subplots()
+ax2.set_title('Class frequencies')
+plt.yscale('log')
+ax2.plot(np.arange(sorted.shape[0]), sorted)
+plt.savefig(f'datasets/imgs/AmazonCat-13K_{DATASET_TYPE}_histogram.png', dpi=163)
+
+# %% [markdown]
+# Next, we can calculate the statistics for class frequencies, title char lengths, content char lengths, and instance class counts.
+
+# %%
+vlen = np.vectorize(len)
+
+stats = [
+    ('title char lengths', get_stats(vlen(df_raw['title']))),
+    ('content char lengths', get_stats(vlen(df_raw['content']))),
+
+    ('class frequencies', get_stats(freqs_before)),
+    ('instance class count', get_stats(df_before['y'].map(len))),
+    ('token lengths', get_stats(vlen(df_before['X']))),
+
+    ('class frequencies (after cutoff)', get_stats(freqs_after)),
+    ('instance class count (after cutoff)', get_stats(df_after['y'].map(len))),
+    ('token lengths (after cutoff)', get_stats(vlen(df_after['X']))),
+]
+
+ds_stats_index, ds_stats = zip(*stats)
+stats_df = pd.DataFrame(ds_stats, index=ds_stats_index)
+stats_df.to_csv(f'datasets/stats/AmazonCat-13K_{DATASET_TYPE}.csv')
+
+
+# %% [markdown]
+# Calculate the labels below a set of frequency thresholds
+
+# %%
+freqs_args = np.argsort(freqs_after)
+
+def freqs_args_below(threshold):
+    # Index before which all indexes point to frequences below the threshold
+    i = np.searchsorted(freqs_after, threshold, side='right', sorter=freqs_args)
+    # return freqs_args[i-1:0:-1]
+    return freqs_args[i-1]
+
+vfreqs_args_below = np.vectorize(freqs_args_below)
+
+# %%
+thresholds = [50, 100, 1_000, 10_000, 50_000, 100_000]
+labels = vfreqs_args_below(thresholds)
+
+threshold_labels = pd.DataFrame(
+    { 'label': labels, 'frequency': freqs_after[labels] },
+    index=pd.Index(thresholds, name='threshold'))
+threshold_labels.to_csv(f'datasets/stats/AmazonCat-13K_{DATASET_TYPE}_thresholds.csv')
