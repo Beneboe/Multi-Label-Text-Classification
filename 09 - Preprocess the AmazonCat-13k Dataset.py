3d774d77e6fc569411270a39a26b511c6a3655b2
Benedikt <benediktbhm@gmail.com>
2020-10-14 13:23:09 +0200

    moved preprocessing to new file

--- 09 - Preprocess the AmazonCat-13k Dataset.py
+++ 09 - Preprocess the AmazonCat-13k Dataset.py
@@ -1,68 +1,7 @@
-# %%
-from utils.text_preprocessing import preprocess
-import pandas as pd
-import numpy as np
+from utils.text_preprocessing import preprocess_amazoncat13k
 
-# %% [markdown]
-# # Preprocess the AmazonCat-13k Dataset
+preprocess_amazoncat13k('trn')
+preprocess_amazoncat13k('tst')
 
-# %%
-CLASS_COUNT = 13330
-DATASET_TYPE = 'trn'
-ADD_CONTENT = False
-# CUTOFF is inclusive
-CUTOFF = 0 if DATASET_TYPE == 'trn' else 0
-DS_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}.json'
-SUFFIX = '.content' if ADD_CONTENT else ''
-BEFORE_CUTOFF_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}{SUFFIX}.before.json'
-AFTER_CUTOFF_PATH = f'datasets/AmazonCat-13K/{DATASET_TYPE}{SUFFIX}.processed.json'
-
-# %% [markdown]
-# First, load the dataset.
-
-# %%
-df = pd.read_json(DS_PATH, lines=True)
-df
-
-# %% [markdown]
-# ## Preprocess the Dataset
-
-# %%
-if ADD_CONTENT:
-    df['title'] = df['title'].str.cat(df['content'], sep=' ')
-X = preprocess(df['title'])
-y = df['target_ind']
-
-# %% [markdown]
-# Save the dataset before the cutoff
-
-df_processed = pd.DataFrame({ 'X': X, 'y': y })
-df_processed.to_json(BEFORE_CUTOFF_PATH, orient='records', lines=True)
-
-# %%
-vlen = np.vectorize(len)
-token_lens = vlen(X)
-
-# %% [markdown]
-# Cut off instances with not enough tokens.
-
-# %%
-print('Instances with less than or equal to {0} tokens get cut off.'.format(CUTOFF))
-count = np.count_nonzero(token_lens <= CUTOFF)
-print('This amounts to {0} instances ({1:.2%}).'.format(
-    count, count / token_lens.shape[0]))
-
-# Cutoff
-indices = np.arange(X.shape[0])[token_lens > CUTOFF]
-X = X[indices]
-y = y[indices]
-
-# %% [markdown]
-# Save the dataset
-
-# %%
-df_processed = pd.DataFrame({ 'X': X, 'y': y })
-df_processed
-
-# %%
-df_processed.to_json(AFTER_CUTOFF_PATH, orient='records', lines=True)
+# preprocess_amazoncat13k('trn', append_content=True)
+# preprocess_amazoncat13k('tst', append_content=True)
--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -1,5 +1,6 @@
 import gensim
 import pandas as pd
+import numpy as np
 from nltk import word_tokenize
 from string import punctuation
 
@@ -26,6 +27,55 @@ def preprocess(series):
     series = series.apply(to_token_id)
 
     return series
+
+def preprocess_amazoncat13k(
+        dataset,
+        dataset_path=None,
+        before_path=None,
+        after_path=None,
+        token_threshold=0,
+        append_content=False):
+    
+    suffix = ''
+    if append_content:
+        suffix = '.content'
+
+    if dataset_path is None:
+        dataset_path = f'datasets/AmazonCat-13K/{dataset}.json'
+    
+    if before_path is None:
+        before_path = f'datasets/AmazonCat-13K/{dataset}{suffix}.before.json'
+    
+    if after_path is None:
+        after_path = f'datasets/AmazonCat-13K/{dataset}{suffix}.processed.json'
+
+    df = pd.read_json(dataset_path, lines=True)
+
+    if append_content:
+        df['title'] = df['title'].str.cat(df['content'], sep=' ')
+
+    # Actual preprocessing
+    X = preprocess(df['title'])
+    y = df['target_ind']
+
+    df_before = pd.DataFrame({ 'X': X, 'y': y })
+    df_before.to_json(before_path, orient='records', lines=True)
+
+    token_lens = X.map(len)
+    remove_indices = np.arange(X.shape[0])[token_lens > token_threshold]
+    remove_count = token_lens.shape[0] - remove_indices.shape[0]
+
+    print('Instances with less than or equal to {0} tokens will be removed.'.format(token_threshold))
+    print('This amounts to {0} instances ({1:.2%}).'.format(remove_count, remove_count / token_lens.shape[0]))
+
+    # Remove instances under token_threshold
+    X = X[remove_indices]
+    y = y[remove_indices]
+
+    df_after = pd.DataFrame({ 'X': X, 'y': y })
+    df_after.to_json(after_path, orient='records', lines=True)
+
+
 
 if __name__ == "__main__":
 
