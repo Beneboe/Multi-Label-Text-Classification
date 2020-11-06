eb78f90fe29e6e0b81bb996f5f6965acb243fe28
Benedikt Böhme <benediktbhm@gmail.com>
2020-11-06 16:52:32 +0100

    more bug fixes

--- 16 - Create Model Training History Diagrams.py
+++ 16 - Create Model Training History Diagrams.py
@@ -1,21 +1,21 @@
+# %%
 from utils.plots import plot_history
+import utils.storage as st
 import matplotlib.pyplot as plt
-import json
 
 # %%
 CLASS = 8842
 
 # %%
-keras_model_class_names = [f'{CLASS}_{name}' for name in ['50%positive', '20%positive', '10%positive', 'unbalanced']]
+type_names = ['50%positive', '20%positive', '10%positive', 'unbalanced']
 
 # %% [markdown]
 # Plot the history diagrams
 
 # %%
-for name in keras_model_class_names:
-    history = None
-    with open(f'results/history/{name}.json', 'r') as fp:
-        history = json.load(fp)
+for type_name in type_names:
+    name = st.get_name(CLASS, type_name)
+    history = st.load_history(CLASS, type_name)
     plot_history(history, name)
     plt.tight_layout()
     plt.savefig(f'datasets/imgs/classifier_{name}_history.png', dpi=163)
