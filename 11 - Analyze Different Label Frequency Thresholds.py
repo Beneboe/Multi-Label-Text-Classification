8dd620b57928a9b8bd5b61014c1ef84ea31fa54c
Benedikt Böhme <benediktbhm@gmail.com>
2020-12-02 15:18:37 +0100

    updated 11

--- 11 - Analyze Different Label Frequency Thresholds.py
+++ 11 - Analyze Different Label Frequency Thresholds.py
@@ -1,9 +1,11 @@
 # %%
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import utils.storage as st
 import utils.metrics as mt
 import utils.dataset as ds
+from itertools import product
 
 # %% [markdown]
 # # Analyze Different Label Frequency Thresholds
@@ -16,34 +18,54 @@ X_test, y_test = ds.import_amazoncat13k('tst', INPUT_LENGTH)
 # Gather the metrics
 
 # %%
-def get_metric_data(label):
-    _, yi_expt = ds.get_dataset(X_test, y_test, label)
-    yi_pred = st.load_prediction(label, '50%positive')
-    return (mt.precision(yi_pred, yi_expt), mt.recall(yi_pred, yi_expt))
+labels = ds.amazoncat13k_threshold_labels
+thresholds = ds.amazoncat13k_thresholds
 
-all_metric_data = [get_metric_data(label) for label in ds.amazoncat13k_threshold_labels]
-precisions, recalls = zip(*all_metric_data)
+ye = np.array([ds.get_dataset(X_test, y_test, label)[1] for label in labels]).T
+
+b_yp = np.array([st.load_prediction(label, '50%positive') for label in labels]).T
+ub_yp = np.array([st.load_prediction(label, 'unbalanced') for label in labels]).T
+
+b_precisions = [mt.precision(b_yp[:, i] , ye[:, i]) for i in range(len(labels))]
+b_recalls = [mt.recall(b_yp[:, i], ye[:, i]) for i in range(len(labels))]
+
+ub_precisions = [mt.precision(ub_yp[:, i] , ye[:, i]) for i in range(len(labels))]
+ub_recalls = [mt.recall(ub_yp[:, i], ye[:, i]) for i in range(len(labels))]
 
 # %% [markdown]
 # Graph the performance of each thresholds
 
 # %%
-formatted_thresholds = ['{:,d}'.format(threshold) for threshold in ds.amazoncat13k_thresholds]
+# formatted_thresholds = ['{:,d}'.format(threshold) for threshold in thresholds]
+formatted_thresholds = ['{:}\n({:,d})'.format(label, thresholds) for label, thresholds in zip(labels, thresholds)]
 
-plt.plot(formatted_thresholds, precisions, 'bs')
-plt.plot(formatted_thresholds, recalls, 'y^')
-plt.title('Classifier Frequency Performance')
-plt.xlabel('Label Frequency Levels')
-plt.legend(['Precision', 'Recall'])
+line1, = plt.plot(formatted_thresholds, b_precisions, 'bs', label = 'Balanced Precision')
+line2, = plt.plot(formatted_thresholds, b_recalls, 'b^', label = 'Balanced Recall')
+# line3, = plt.plot(formatted_thresholds, ub_precisions, 'ys', label = 'Unbalanced Precision')
+# line4, = plt.plot(formatted_thresholds, ub_recalls, 'y^', label = 'Unbalanced Recall')
+
+plt.title('L2 Deconstructed Performance')
+plt.xlabel('Labels of L2')
+plt.ylabel('Metric Performance')
+plt.legend()
 plt.grid()
-plt.savefig('results/imgs/label_frequency_performance.png', dpi=163)
-plt.savefig('results/imgs/label_frequency_performance.pdf')
+
+plt.savefig('results/imgs/l2_deconstructed_performance.png', dpi=163)
+plt.savefig('results/imgs/l2_deconstructed_performance.pdf')
 plt.show()
 
 # %%
 # Create the table for each of the thresholds
 
-table = pd.DataFrame(all_metric_data, index=formatted_thresholds, columns=['precision', 'recall'])
-table.to_csv('results/imgs_data/label_frequency_performance.csv')
+table = pd.DataFrame({
+    'label': labels,
+    'threshold': thresholds,
+    'balanced precision': b_precisions,
+    'balanced recall': b_recalls,
+    'unbalanced precision': ub_precisions,
+    'unbalanced recall': ub_recalls,
+})
+table.to_csv('results/imgs_data/l2_deconstructed_performance.csv')
+table
 
 # %%
