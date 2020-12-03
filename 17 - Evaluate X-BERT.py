d49424ed1a789f033e2a05f35163fbf5c2bb0119
Benedikt Böhme <benediktbhm@gmail.com>
2020-12-03 18:55:48 +0100

    updated 17

--- 17 - Evaluate X-BERT.py
+++ 17 - Evaluate X-BERT.py
@@ -5,10 +5,9 @@ import utils.metrics as mt
 import utils.dataset as ds
 import utils.storage as st
 import numpy as np
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, auc
 
 # %%
-
 # load the test set
 INPUT_LENGTH = 10
 X_test, y_test = ds.import_amazoncat13k('tst', INPUT_LENGTH)
@@ -75,25 +74,26 @@ print(f'Macro f1score {macro}')
 print(f'Micro f1score {micro}')
 
 # %%
-ova_y_predict = np.array([st.load_prediction(label, 'unbalanced') for label in labels]).T
+cga_y_predict = np.array([st.load_prediction(label, 'unbalanced') for label in labels]).T
 # Cannot use the same y_expect for xbert and ova because the test set was shuffled when the prediction for ova was
 # calculated and therefore the same shuffling needs to apply to y_expect
-ova_y_expect = np.array([ds.get_dataset(X_test, y_test, label)[1] for label in labels]).T
+cga_y_expect = np.array([ds.get_dataset(X_test, y_test, label)[1] for label in labels]).T
 
 # %% [markdown]
 # ## Create the ROC curve
 
 # %%
-fpr, tpr = dict(), dict()
-fpr['xbert'], tpr['xbert'], _ = roc_curve(xbert_y_expect.ravel(), xbert_y_predict.ravel())
-fpr['ova'], tpr['ova'], _ = roc_curve(ova_y_expect.ravel(), ova_y_predict.ravel())
+curve_names = ['X-BERT', 'Coarse']
+curves = [
+    roc_curve(xbert_y_expect.ravel(), xbert_y_predict.ravel()),
+    roc_curve(cga_y_expect.ravel(), cga_y_predict.ravel())
+]
+areas = [auc(fpr, tpr) for fpr, tpr, _ in curves]
 
 plt.figure()
 lw = 2
-plt.plot(fpr['xbert'], tpr['xbert'], color='darkorange',
-         lw=lw, label='X-BERT prediction')
-plt.plot(fpr['ova'], tpr['ova'], color='aqua',
-         lw=lw, label='Independent Classifier (unbalanced training)')
+for name, (fpr, tpr, _), area in zip(curve_names, curves, areas):
+    plt.plot(fpr, tpr, lw=lw, label=f'%s (area = %0.2f)' % (name, area))
 plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
@@ -101,9 +101,8 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic')
 plt.legend(loc="lower right")
-
-plt.savefig(f'results/imgs/roc_curve.png', dpi=163)
-plt.savefig(f'results/imgs/roc_curve.pdf')
+plt.savefig(f'results/imgs/xbert_curve.png', dpi=163)
+plt.savefig(f'results/imgs/xbert_curve.pdf')
 plt.show()
 
 # %%
